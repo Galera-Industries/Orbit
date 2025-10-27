ee0587fa929948ec0d4f802afa2528eb9dc94f44
mmrdr <isaev2k18@gmail.com>
2025-10-27 16:37:13 +0300

    Created mock for coredata

---
+++ Orbit/Mocks/CoreDataMock.swift
@@ -0,0 +1,28 @@
+//
+//  CoreDataMock.swift
+//  Orbit
+//
+//  Created by Кирилл Исаев on 27.10.2025.
+//
+
+import Foundation
+
+final class CoreDataMock: CoreDataProtocol {
+    var storageCount: Int
+    var fetchAllResult: [CDClipboardItem] = []
+    var createItemResult: [ClipboardItem] = []
+    var deleteItemResult: [ClipboardItem] = []
+    var fetchByOrderResult: CDClipboardItem?
+    
+    init(storageCount: Int) {
+        self.storageCount = storageCount
+    }
+    
+    func createItem(_ item: ClipboardItem) { createItemResult.append(item) }
+    
+    func deleteItem(_ item: ClipboardItem) { deleteItemResult.append(item) }
+    
+    func fetchAll() -> [CDClipboardItem] { fetchAllResult }
+    
+    func fetch(byOrder: Int) -> CDClipboardItem? { fetchByOrderResult }
+}
---
+++ OrbitTests/ClipboardTests.swift
@@ -0,0 +1,12 @@
+//
+//  ClipboardTests.swift
+//  OrbitTests
+//
+//  Created by Кирилл Исаев on 27.10.2025.
+//
+
+import XCTest
+
+final class ClipboardTests: XCTestCase {
+    
+}
