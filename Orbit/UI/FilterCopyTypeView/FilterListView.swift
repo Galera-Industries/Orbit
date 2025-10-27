0fd170433bdccb07f7dffb4d72f287630e991eb2
mmrdr <isaev2k18@gmail.com>
2025-10-27 15:41:55 +0300

    Created ui for filter

---
+++ Orbit/UI/FilterCopyTypeView/FilterListView.swift
@@ -0,0 +1,8 @@
+//
+//  FilterListView.swift
+//  Orbit
+//
+//  Created by Кирилл Исаев on 27.10.2025.
+//
+
+import Foundation
---
+++ Orbit/UI/FilterCopyTypeView/FilterRow.swift
@@ -0,0 +1,8 @@
+//
+//  FilterRow.swift
+//  Orbit
+//
+//  Created by Кирилл Исаев on 27.10.2025.
+//
+
+import Foundation
