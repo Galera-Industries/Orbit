73ad363b63181d054c9c9213a78b4d5fa470a2f2
Владислав Панкратов <pankratovvlad1@gmail.com>
2025-11-08 04:19:38 +0300

    Add setShellModel to switch mode

--- Orbit/Core/ModuleRegistry.swift
+++ Orbit/Core/ModuleRegistry.swift
@@ -10,6 +10,7 @@ import Foundation
 final class ModuleRegistry {
     private var modules: [AppMode: ModulePlugin] = [:]
     private let context: ModuleContext
+    private weak var shellModel: ShellModel?
     
     init(context: ModuleContext) {
         self.context = context
@@ -18,6 +19,9 @@ final class ModuleRegistry {
     func register(_ module: ModulePlugin) {
         modules[module.mode] = module
         module.activate(context: context)
+        if let shell = shellModel {
+            module.setShellModel(shell)
+        }
     }
     
     func module(for mode: AppMode) -> ModulePlugin? {
@@ -26,5 +30,12 @@ final class ModuleRegistry {
     
     func deactivateAll() {
         modules.values.forEach { $0.deactivate() }
+    }
+    
+    func setShellModel(_ shell: ShellModel) {
+        self.shellModel = shell
+        for module in modules.values {
+            module.setShellModel(shell)
+        }
     }
 }
